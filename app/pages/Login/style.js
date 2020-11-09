2ae60615e9bf98fa36c50fac3aff44d764abcbc6
sugarhaining <sugarhaining@163.com>
2020-11-09 22:15:35 +0800

    feat: add unit adaptation

---
+++ app/pages/Login/style.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+import { layout, pxToDp, deviceHeightDp, deviceWidthDp } from '../../style'
+
+export default StyleSheet.create({
+    container: {
+        width: deviceWidthDp,
+        height: deviceHeightDp,
+        flex: 1,
+        alignItems: 'center',
+    },
+    logo: {
+        width: pxToDp(188),
+        height: pxToDp(188),
+        backgroundColor: 'white',
+        ...layout.border(pxToDp(1), 'solid', 'black'),
+        borderRadius: pxToDp(188 / 2),
+        marginTop: pxToDp(20)
+    }
+})
