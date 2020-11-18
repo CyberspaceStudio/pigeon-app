5c8af50455029e9ad36ac4621d5e81b0fc730ed0
sugarhaining <sugarhaining@163.com>
2020-11-18 23:24:20 +0800

    feat: support mutiple post data enctype

--- app/utils/CONSTANT.js
+++ app/utils/CONSTANT.js
@@ -1,1 +1,1 @@
-export const BASE_FETCH_URL_PREFIX = 'http://...'
+export const BASE_FETCH_URL_PREFIX = 'https://minimalist.net.cn/pigeon'
--- app/utils/fetchWrap.js
+++ app/utils/fetchWrap.js
@@ -1,33 +1,39 @@
 import { BASE_FETCH_URL_PREFIX } from './CONSTANT'
 
-const payload2url = (payload) => {
+const payload2url = (data) => {
   const dataSet = []
 
-  Object.entries(payload).forEach(([key, value]) => {
+  Object.entries(data).forEach(([key, value]) => {
     dataSet.push(`${key}=${value}`)
   })
 
   return `?${dataSet.join('&')}`
 }
 
-const payload2body = (payload) => JSON.stringify(payload)
+const payload2body = (data) => JSON.stringify(data)
 
 const isGetFetch = (method) => method === 'GET'
 
+/*
+ * payload: {data: Object, contentType: String, ...}
+ *
+ *
+ */
 export const fetchWrap = async (path, method, payload) => {
+  const { data, contentType = 'x-www-form-urlencoded' } = payload
   let err, res, body
 
   if (isGetFetch(method)) {
-    path = `${BASE_FETCH_URL_PREFIX}${path}${payload2url(payload)}`
+    path = `${BASE_FETCH_URL_PREFIX}${path}${payload2url(data)}`
   } else {
-    body = payload2body(payload)
+    body = payload2body(data)
   }
 
   try {
     res = await fetch(path, {
       method,
       headers: {
-        'Content-type': 'application/json'
+        'Content-type': contentType
       },
       body
     }).then((res) => res.json())
